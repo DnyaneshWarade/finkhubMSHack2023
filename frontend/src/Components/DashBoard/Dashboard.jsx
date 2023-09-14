2f1b3314df09c6d71312736bc82e9e2a6b188d64
joshivilas <122909628+joshivilas@users.noreply.github.com>
2023-09-14 14:28:41 +0530

    update. dashboard
    Updated the dashboard logics and style.

--- frontend/src/Components/DashBoard/Dashboard.jsx
+++ frontend/src/Components/DashBoard/Dashboard.jsx
@@ -5,10 +5,11 @@ import "./Dashboard.css";
 import ProfileIcon from "./../../Images/ProfileIcon.png";
 
 export default function Dashboard() {
-  const [userType, setUserType] = useState("");
-  const [action, setAction] = useState("");
+  const [userType, setUserType] = useState("PulseTradeAI");
+  const [action, setAction] = useState("Buy");
   const [stock, setStock] = useState("");
   const [amount, setAmount] = useState("");
+  const [expectedAmount, setExpectedAmount] = useState(0);
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(0);
   const [exProfit, setExpectedProfit] = useState("");
@@ -25,93 +26,117 @@ export default function Dashboard() {
     return `${key} - ${companies[key]}`;
   });
 
+  const handleExpectedAmount = () => {
+    console.log(minPrice, expectedAmount, maxPrice)
+    if (minPrice > expectedAmount || maxPrice < expectedAmount) {
+      alert("Should be with min and max");
+      return;
+    }
+    setExpectedAmount(expectedAmount);
+    alert("Saved")
+  }
+
   return (
     <div className="DashBoard">
-      <div className="FirstLine">
-        I am a StockFink user of
-        <div className="RadioSwitch">
-          <input
-            type="radio"
-            value="PulseTradeAI"
-            name="userType"
-            id="PulseTradeAI"
-            onChange={(e) => setUserType(e.target.value)}
-          />
-          <label htmlFor="PulseTradeAI">PulseTradeAI</label>
-          <input
-            type="radio"
-            value="FinkAIBeta"
-            name="userType"
-            id="FinkAIBeta"
-            onChange={(e) => setUserType(e.target.value)}
-          />{" "}
-          <label htmlFor="FinkAIBeta">FinkAIBeta</label>
+      <div className="Script">
+        <div className="FirstLine">
+          I am a StockFink user of
+          <div className="RadioSwitch">
+            <input
+              type="radio"
+              value="PulseTradeAI"
+              name="userType"
+              id="PulseTradeAI"
+              onChange={(e) => setUserType(e.target.value)}
+              defaultChecked
+            />
+            <label htmlFor="PulseTradeAI">PulseTradeAI</label>
+            <input
+              type="radio"
+              value="FinkAIBeta"
+              name="userType"
+              id="FinkAIBeta"
+              onChange={(e) => setUserType(e.target.value)}
+            />{" "}
+            <label htmlFor="FinkAIBeta">FinkAIBeta</label>
+          </div>
+        </div>
+
+        <div className="SecondLine">
+          {" "}
+          And I have decided to
+          <div className="RadioSwitch2">
+            <input
+              type="radio"
+              value="Buy"
+              name="action"
+              id="buy"
+              onChange={(e) => setAction(e.target.value)}
+              defaultChecked
+            />
+            <label htmlFor="buy">Buy</label>
+
+            <input
+              type="radio"
+              value="Sell"
+              name="action"
+              id="sell"
+              onChange={(e) => setAction(e.target.value)}
+            />
+            <label htmlFor="sell">Sell</label>
+          </div>
+          <span className="StockSelect">
+            <input
+              type="text"
+              list="CompanyList"
+              value={stock}
+              onChange={(e) => setStock(e.target.value)}
+              className="StockSelectInput"
+              placeholder="Write a stock name"
+            />
+            <datalist id="CompanyList">
+              {Object.keys(companies).map((key) => (
+                <option value={companies[key]} key={key}>
+                  {key}-{companies[key]}
+                </option>
+              ))}
+            </datalist>
+          </span>
+        </div>
+
+        <div className="ThirdLine">
+          <div className="PriceSelection">
+            at
+            <input
+              type="number"
+              id="amount"
+              max="100000"
+              onChange={(e) => setAmount(e.target.value)}
+            />{" "}
+            <span className="Currency">USD</span>
+          </div>
+
+          <div className="Prediction">
+            <input
+              type="number"
+              id="amount"
+              max="100000"
+              onChange={(e) => setExpectedAmount(e.target.value)}
+              value={expectedAmount}
+            />
+            <span className="Currency">USD</span>
+            <div className="value-1">Min: {minPrice === 0 ? "" : minPrice}</div>
+            <div className="value-2">Max: {maxPrice === 0 ? "" : maxPrice}</div>
+          </div>
         </div>
       </div>
-
-      <div className="SecondLine">
-        {" "}
-        And I have decided to
-        <div className="RadioSwitch">
-          <input
-            type="radio"
-            value="Buy"
-            name="action"
-            id="buy"
-            onChange={(e) => setAction(e.target.value)}
-          />
-          <label htmlFor="buy">Buy</label>
-
-          <input
-            type="radio"
-            value="Sell"
-            name="action"
-            id="sell"
-            onChange={(e) => setAction(e.target.value)}
-          />
-          <label htmlFor="sell">Sell</label>
-        </div>
-        <span className="StockSelect">
-          <input
-            type="text"
-            list="CompanyList"
-            value={stock}
-            onChange={(e) => setStock(e.target.value)}
-            className="StockSelectInput"
-            placeholder="Write a stock name"
-          />
-          <datalist id="CompanyList">
-            {Object.keys(companies).map((key) => (
-              <option value={companies[key]}>
-                {key}-{companies[key]}
-              </option>
-            ))}
-          </datalist>
-        </span>
-      </div>
-      <div className="ThirdLine">
-        {" "}
-        <div className="PriceSelection">
-          at
-          <input
-            type="number"
-            id="amount"
-            max="100000"
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          <span className="Currency">USD</span>
-          <div class="value-1">Min: {minPrice === 0 ? "" : minPrice}</div>
-          <div class="value-2">Max: {maxPrice === 0 ? "" : maxPrice}</div>
-        </div>
-      </div>
-
       <div className="Preview">
         <p className="PreviewHeading">Preview</p>
         <div className="PreviewDescription">
           <img src={ProfileIcon} alt="icon" />
           <p>{preview}</p>
         </div>
-        <div className="PostButton">
+        <div className="PostButton" onClick={handleExpectedAmount}>
           <p>Post</p>
         </div>
       </div>
