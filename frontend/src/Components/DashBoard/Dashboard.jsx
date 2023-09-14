6b7a7faa15eb50d7191b3a09ac1576df4c4c4ce3
joshivilas <122909628+joshivilas@users.noreply.github.com>
2023-09-14 12:47:35 +0530

    update
    Updated the  css and structure of dashboard.

--- frontend/src/Components/DashBoard/Dashboard.jsx
+++ frontend/src/Components/DashBoard/Dashboard.jsx
@@ -27,77 +27,84 @@ export default function Dashboard() {
 
   return (
     <div className="DashBoard">
-      I am a StockFink user of
-      <div className="RadioSwitch">
-        <input
-          type="radio"
-          value="PulseTradeAI"
-          name="userType"
-          id="PulseTradeAI"
-          onChange={(e) => setUserType(e.target.value)}
-        />
-        <label htmlFor="PulseTradeAI">PulseTradeAI</label>
-        <input
-          type="radio"
-          value="FinkAIBeta"
-          name="userType"
-          id="FinkAIBeta"
-          onChange={(e) => setUserType(e.target.value)}
-        />{" "}
-        <label htmlFor="FinkAIBeta">FinkAIBeta</label>
+      <div className="FirstLine">
+        I am a StockFink user of
+        <div className="RadioSwitch">
+          <input
+            type="radio"
+            value="PulseTradeAI"
+            name="userType"
+            id="PulseTradeAI"
+            onChange={(e) => setUserType(e.target.value)}
+          />
+          <label htmlFor="PulseTradeAI">PulseTradeAI</label>
+          <input
+            type="radio"
+            value="FinkAIBeta"
+            name="userType"
+            id="FinkAIBeta"
+            onChange={(e) => setUserType(e.target.value)}
+          />{" "}
+          <label htmlFor="FinkAIBeta">FinkAIBeta</label>
+        </div>
       </div>
-      <br />
-      And I have decided to
-      <div className="RadioSwitch">
-        <input
-          type="radio"
-          value="Buy"
-          name="action"
-          id="buy"
-          onChange={(e) => setAction(e.target.value)}
-        />
-        <label htmlFor="buy">Buy</label>
 
-        <input
-          type="radio"
-          value="Sell"
-          name="action"
-          id="sell"
-          onChange={(e) => setAction(e.target.value)}
-        />
-        <label htmlFor="sell">Sell</label>
+      <div className="SecondLine">
+        {" "}
+        And I have decided to
+        <div className="RadioSwitch">
+          <input
+            type="radio"
+            value="Buy"
+            name="action"
+            id="buy"
+            onChange={(e) => setAction(e.target.value)}
+          />
+          <label htmlFor="buy">Buy</label>
+
+          <input
+            type="radio"
+            value="Sell"
+            name="action"
+            id="sell"
+            onChange={(e) => setAction(e.target.value)}
+          />
+          <label htmlFor="sell">Sell</label>
+        </div>
+        <span className="StockSelect">
+          <input
+            type="text"
+            list="CompanyList"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+            className="StockSelectInput"
+            placeholder="Write a stock name"
+          />
+          <datalist id="CompanyList">
+            {Object.keys(companies).map((key) => (
+              <option value={companies[key]}>
+                {key}-{companies[key]}
+              </option>
+            ))}
+          </datalist>
+        </span>
       </div>
-      <span className="StockSelect">
-        <input
-          type="text"
-          list="CompanyList"
-          value={stock}
-          onChange={(e) => setStock(e.target.value)}
-          className="StockSelectInput"
-          placeholder="Write a stock name"
-        />
-        <datalist id="CompanyList">
-          {Object.keys(companies).map((key) => (
-            <option value={companies[key]}>
-              {key}-{companies[key]}
-            </option>
-          ))}
-        </datalist>
-      </span>
-      <br />
-      <div className="PriceSelection">
-        at
-        <input
-          type="number"
-          id="amount"
-          max="100000"
-          onChange={(e) => setAmount(e.target.value)}
-        />
-        <span className="Currency">USD</span>
-        <div class="value-1">Min: {minPrice === 0 ? "" : minPrice}</div>
-        <div class="value-2">Max: {maxPrice === 0 ? "" : maxPrice}</div>
+      <div className="ThirdLine">
+        {" "}
+        <div className="PriceSelection">
+          at
+          <input
+            type="number"
+            id="amount"
+            max="100000"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <span className="Currency">USD</span>
+          <div class="value-1">Min: {minPrice === 0 ? "" : minPrice}</div>
+          <div class="value-2">Max: {maxPrice === 0 ? "" : maxPrice}</div>
+        </div>
       </div>
-      <br />
+
       <div className="Preview">
         <p className="PreviewHeading">Preview</p>
         <div className="PreviewDescription">
